9bba5e6ee70262afa7ba77134de837b8a3c4a91e
Ahuiwei <Florentwei@outlook.com>
2017-12-31 16:49:17 +0800

    resource.h

---
+++ paint_text/resource.h
@@ -0,0 +1,37 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by paint.rc
+//
+#define IDI_TITLE                       101
+#define IDM_FILE_NEW                    40001
+#define IDM_FILE_OPEN                   40002
+#define IDM_FILE_SAVE                   40003
+#define IDM_APP_EXIT                    40004
+#define IDM_EDIT_CHOOSE                 40005
+#define IDM_EDIT_DELETE                 40006
+#define IDM_EDIT_ZOOMIN                 40007
+#define IDM_EDIT_ZOOMOUT                40008
+#define IDM_EDIT_SETCOLOR               40009
+#define IDM_TOOL_LINE                   40010
+#define IDM_TOOL_ELLIPSE                40011
+#define IDM_TOOL_RECT                   40012
+#define IDM_TOOL_POLYGON                40013
+#define IDM_HELP_ABOUT                  40014
+#define IDM_TOOL_ELLIPSE1               40015
+#define IDM_WIDE_ONE                    40016
+#define IDM_WIDE_TWO                    40017
+#define IDM_WIDE_THREE                  40018
+#define IDM_TOOL_RECT1                  40019
+#define IDM_EDIT_SETCOLOR_FULL          40020
+#define IDM_EDIT_MOVE                   40021
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
